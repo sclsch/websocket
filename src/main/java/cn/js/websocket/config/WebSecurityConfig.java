3d02c39e3064ee159763032bee5be6c36a8de678
sunchengliang <258268936@qq.com>
2022-11-24 14:26:36 +0800

    ....

--- src/main/java/cn/js/websocket/config/WebSecurityConfig.java
+++ src/main/java/cn/js/websocket/config/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package cn.js.websocket.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @author JiaShun
@@ -53,6 +55,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("Janet").password("freedom").roles("USER");
     }
+
+
+
 
     /**
      * /resources/static/目录下的静态资源，Spring Security不拦截
